567e1df577f2b5164c0e295b407523224872c23f
Onyme <jocorl@hotmail.ch>
2022-11-05 20:11:39 +0100

    relecture à faire

--- crypto_serie_5.py
+++ crypto_serie_5.py
@@ -3,15 +3,13 @@ from DES_tables import *
 
 #Initial Permutation
 def Initialization(Input):
-    output = [] 
+    output = [2] * len(IP)
 
-    for x in range(len(Input)):
-        output.insert(IP[x]-1, Input[x])
-        #output[IP[x]-1] = Input[x]
+    for x in range(len(Input)):        
+        output[x] =  Input[IP[x]-1]
 
-    half = int(len(Input)/2)
-    left_half = output[0:half]
-    right_half = output[half:len(IP)+1]
+    left_half = output[:32]
+    right_half = output[32:]
 
     return left_half,right_half
 
@@ -21,50 +19,55 @@ def Initialization(Input):
 def key_generation(Key):
 
     keys= []
+    output = [2] * len(Key)
+
+    for x in range(len(PC_1)):
+        output[x] = Key[PC_1[x]-1]
+        
+    
+    left_half = output[0:28]
+    right_half = output[28:56]
+
+
 
     for rotation in Rotations:
         #key permutation pc_1
-        output = Key
-        #key = [2] * 48
-        key = []
-
-        for x in range(len(PC_1)):
-            output[PC_1[x]-1] = Key[x]
-
-        left_half = output[0:28]
-        right_half = output[28:56]
-
+        key = [2] * len(PC_2)
 
 
         #binary rotation
         left = left_half[rotation:] + left_half[0:rotation]
         right = right_half[rotation:] + right_half[0:rotation]
-        
+
         for i in range(len(right_half)):
             right = right_half
             right[i-rotation] = right_half[i]
 
+        for i in range(len(left_half)):
+            left = left_half
+            left[i-rotation] = left_half[i]
+
         full = left + right
 
-        for x in range(len(PC_2)):
-            key.insert(PC_2[x]-1,full[x])
 
+        for x in range(len(PC_2)):
+            key[x] = full[PC_2[x]-1]
         keys.append(key)
     return keys
 
 
 """======================================================================================="""
-"""!!!!!!!!!!!!! conversion to INT !!!!!!!!!!!!!!!!!!!"""
 
 def cipher_function(right_half, keys):
     
-    R = [0] * len(E)
+    R = [2] * len(E)
     output = ""
     
+
     #1
     for x in range(len(E)):
-        R[E[x]-1] = right_half[x % (len(right_half))]
-    #print(len(output))
+        right_half[x % (len(right_half))]
+        R[x] =  right_half[E[x]-1]
 
     #2    
     for x in range(len(R)):
@@ -82,24 +85,36 @@ def cipher_function(right_half, keys):
 
     A_replacement = ""
 
+
     for x in range(len(A_list)):
+        
         y_value = int(A_list[x][0] + A_list[x][5],2)
         x_value = int(A_list[x][1:5],2)
-        #print(y_value)
-        #print(x_value)
-
 
         int_value_replacement = S_Boxes[x][y_value][x_value]
         bin_value_replacement = bin(int_value_replacement).replace("0b","") 
 
         while len(bin_value_replacement) < 4:
             bin_value_replacement = '0' + bin_value_replacement
+        
         A_replacement += bin_value_replacement
+   
     #4
-    final = []
-    
+    final = [2] * len(P)
+
     for x in range(len(P)):
-        final.insert(P[x]-1, A_replacement[x])
+        #print()
+        #print()
+        #print()
+        #print()
+
+        final[x] = A_replacement[P[x]-1]
+
+    #for ele in final:
+    #    if ele == 2:
+    #        final.remove(ele)
+
+
 
     return final
 
@@ -108,33 +123,37 @@ def cipher_function(right_half, keys):
 
 def encryption(Input,Key):
     x = 0
-    key = key_generation(Key)                # génération des clés 
+    key = key_generation(Key)                # génération des clés (liste)
 
     L, R = Initialization(Input) # première séparation  
 
     #premier round
-    while x < 16:
-        print("YYY")
+    while x < 15:
         Rxor = cipher_function(R,key[x])
         Lfinal = []
-        print(Rxor)
-        print(L)
 
 
         for i in range(len(Rxor)):
             Lfinal.append(L[i] ^ int(Rxor[i]))
         
-        print(Lfinal)
         L = R
         R = Lfinal
         x+=1
 
-    #Final permutation
-    LplusR = L + R
-    output_final = []   
+    #round 16
+    Rxor = cipher_function(R,key[x])
+    Lfinal = []
 
-    for x in range(len(LplusR)):
-        output_final.insert(IP_Inverse[x]-1, LplusR[x])
+
+    for i in range(len(Rxor)):
+        Lfinal.append(L[i] ^ int(Rxor[i]))
+
+    #Final permutation
+    LplusR = Lfinal + R
+    output_final = [2] * len(IP_Inverse)
+
+    for x in range(len(IP_Inverse)):
+        output_final[x] = LplusR[IP_Inverse[x]-1]
 
     print("\n===============================================================================\n")
     print(output_final)
